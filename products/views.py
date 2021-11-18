41cf17bf299ad69ecea8182c1260623e3379d61a
reidycolm <reidycolm@gmail.com>
2021-11-18 13:55:21 +0000

    added functionality to delete product

--- products/views.py
+++ products/views.py
@@ -78,7 +78,7 @@ def add_product(request):
         if form.is_valid():
             product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request,
                            'Could not add product to site. \
@@ -118,3 +118,12 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ deletes a product on the site """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+
+    return redirect(reverse('products'))
