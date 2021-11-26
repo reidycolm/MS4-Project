a287cba222644f55ed3221b323f625bb08b5797e
reidycolm <reidycolm@gmail.com>
2021-11-26 12:11:59 +0000

    set up blog model and views

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_blog, name='view_blog'),
+    path('', views.blog, name='blog'),
+    path('blog_post/<slug:slug>/', views.blog_post, name='blog_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
 # Create your views here.
 
-def view_blog(request):
-    return render(request, 'blog/blog.html')
+def blog(request):
+    posts = Post.objects.filter(status=1).order_by('-created_on')
+    template = 'blog/blog.html'
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, template, context)
+
+
+def blog_post(request, slug):
+    post = Post.objects.get(slug=slug)
+    template = 'blog/blog_details.html'
+    context = {
+        'post': post,
+    }
+
+    return render(request, template, context)
+
