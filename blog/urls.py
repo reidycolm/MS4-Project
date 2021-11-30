8bba54850be22261394773bdc64979cce4b2c84d
reidycolm <reidycolm@gmail.com>
2021-11-30 12:49:55 +0000

    added ability for superuser to add new blog post to blog section

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.blog, name='blog'),
     path('blog_post/<slug:slug>/', views.blog_post, name='blog_post'),
+    path('add_blog_post/', views.add_blog_post, name="add_blog_post"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.views import generic
 from .models import Post
+from .forms import PostForm
+from django.contrib import messages
 
-# Create your views here.
 
 def blog(request):
     posts = Post.objects.filter(status=1).order_by('-created_on')
@@ -23,3 +24,31 @@ def blog_post(request, slug):
 
     return render(request, template, context)
 
+
+def add_blog_post(request):
+    if not request.user.is_superuser:
+        messages.error(request,
+                       'Sorry, only Admins can create a new blog post')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            form.save()
+            messages.success(request, 'Successfully added a New Blog Post!')
+            return redirect(reverse('blog_post', args=[post.slug]))
+        else:
+            messages.error(request,
+                           'Could not add your new post to the site. \
+                           Please ensure form is valid!')
+    else:
+        form = PostForm()
+
+    template = 'blog/add_blog_post.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
