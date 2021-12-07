aa5485e0c40a82ab2c62bf5df526518e446ecbae
reidycolm <reidycolm@gmail.com>
2021-12-07 12:38:41 +0000

    deployment part 2

--- hoop_dreams/settings.py
+++ hoop_dreams/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-@m1cs*8=ry=d+42o3_r92kwn6kat(a5u*n#)!rpy1garj)h6x_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hoop-dreams.herokuapp.com']
 
 
 # Application definition
@@ -121,12 +121,17 @@ WSGI_APPLICATION = 'hoop_dreams.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
